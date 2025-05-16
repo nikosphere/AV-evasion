5eccbf1faa3d89c4f161826d0473facd548edea5
danny <danny@DEVMACHINE>
2025-05-15 19:31:50 -0700

    added_amsi_funcnames

---
+++ MerryHollows/EvasionClasss/AMSIByp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerryHollows.EvasionClasss
+{
+    internal class AMSIByp
+    {
+        public static void PatchAmsiScanBuffer()
+        {
+
+        }
+
+        public static void PatchAmsiScanString()
+        {
+
+        }
+
+        public static void PatchEtw()
+        {
+
+        }
+    }
+}
